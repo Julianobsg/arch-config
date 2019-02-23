52b9f8248f23a32d85e574100cf4f2fa6a3e5207
Juliano <julianobsg@gmail.com>
2019-02-23 20:27:03 -0300

    Add swap partition on disk

--- install.sh
+++ install.sh
@@ -3,13 +3,18 @@
 DISK='/dev/sda'
 
 format_disk() {
+  local memory=vmstat -s -S M | grep 'total memory' | tr -dc '0-9'
+  local memory_addr=$(($memory + 100))M
+
   echo "Formatting disk"
   parted -s "$DISK" \
     mklabel msdos \
-    mkpart primary ext2 1 100M \
-    mkpart primary ext2 100M 100% \
+    mkpart primary ext4 1 100M \
+    mkpart primary ext4 $memory_addr 100% \
+    mkpart primary linux-swap 100M  $memory_addr\
     set 1 boot on \
     set 2 LVM on
+    set 3 SWAP on
 }
 
 install_arch() {
