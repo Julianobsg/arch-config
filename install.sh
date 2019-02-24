044ac4a9b81d668d23fa156df0892464512b7a96
Juliano <julianobsg@gmail.com>
2019-02-23 21:59:13 -0300

    Refactor add boot size as variable

--- install.sh
+++ install.sh
@@ -3,15 +3,16 @@
 DISK='/dev/sda'
 
 format_disk() {
+  local boot_size=256
   local memory=vmstat -s -S M | grep 'total memory' | tr -dc '0-9'
-  local memory_addr=$(($memory + 100))M
+  local memory_addr=$(($memory + $boot_size))M
 
   echo "Formatting disk"
   parted -s "$DISK" \
     mklabel msdos \
-    mkpart primary ext4 1 100M \
+    mkpart primary ext2 1 "$boot_size"M \
     mkpart primary ext4 $memory_addr 100% \
-    mkpart primary linux-swap 100M  $memory_addr\
+    mkpart primary linux-swap "$boot_size"M  $memory_addr\
     set 1 boot on \
     set 2 LVM on
     set 3 SWAP on
