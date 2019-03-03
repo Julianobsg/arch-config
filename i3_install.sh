e62e603dcae09d2c9572246fd493c602e37a0a7f
Juliano <julianobsg@gmail.com>
2019-03-02 23:13:16 -0300

    Conf base i3

--- i3_install.sh
+++ i3_install.sh
@@ -4,10 +4,22 @@ VIDEO_DRIVER=xf86-video-intel libva-intel-driver
 install_x(){
   yes | pacman -S $VIDEO_DRIVER
   pacman -S --noconfirm xorg-apps xorg-server xorg-xinit xterm
+  Xorg :0 -configure
+  cp /root/xorg.conf.new /etc/X11/xorg.conf
 }
 
 install_i3() {
-  yes | pacman -S i3-wm
+  yes | pacman -S i3-wm i3status dmenu ttf-dejavu
+  cat > ~/.xinitrc <<EOF
+#!/bin/bash
+exec i3
+EOF
+
+  cat >> /etc/profile  << EOL
+if [[ "$(tty)" == '/dev/tty1' ]]; then
+  exec startx
+fi
+EOL
 }
 
 install_x
