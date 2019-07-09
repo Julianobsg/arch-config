2005cd05d668b45b9e0aee090d5ced6254c6627f
Juliano <julianobsg@gmail.com>
2019-07-09 17:23:35 -0300

    Avoid using password for sudo

--- i3_install.sh
+++ i3_install.sh
@@ -2,12 +2,12 @@
 VIDEO_DRIVER='xf86-video-intel libva-intel-driver'
 
 install_x(){
-  yes | sudo pacman -S $VIDEO_DRIVER
-  sudo pacman -S --noconfirm xorg-apps xorg-server xorg-xinit xterm
+  yes | sudo -i pacman -S $VIDEO_DRIVER
+  sudo -i pacman -S --noconfirm xorg-apps xorg-server xorg-xinit xterm
 }
 
 install_lightdm() {
-  yes | sudo pacman -S lightdm lightdm-gtk-greeter
+  yes | sudo -i pacman -S lightdm lightdm-gtk-greeter
   systemctl enable lightdm.service -f
 }
 
@@ -22,11 +22,11 @@ install_yay() {
 }
 
 install_i3() {
-  yes | sudo pacman -S i3-wm i3status dmenu ttf-dejavu ttf-inconsolata dunst libnotify xdg-utils gnome-icon-theme
+  yes | sudo -i pacman -S i3-wm i3status dmenu ttf-dejavu ttf-inconsolata dunst libnotify xdg-utils gnome-icon-theme
 
   install_lightdm
 
-  yes | sudo pacman -S rxvt-unicode git vim ranger
+  yes | sudo -i pacman -S rxvt-unicode git vim ranger
   install_yay
   yay --noconfirm -S google-chrome urxvt-tabbedex ttf-meslo ttf-mensch otf-inconsolata-dz-powerline
 }
