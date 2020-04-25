7f899708f64109d11a6c234736e105f2bed2bb07
Juliano Gomes <juliano.gomes@tuneduc.com.br>
2020-04-25 18:21:42 -0300

    Update polybar for multiple monitors

--- dotfiles/.config/i3/polybar.sh
+++ dotfiles/.config/i3/polybar.sh
@@ -7,4 +7,10 @@ killall -q polybar
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch polybar
-polybar example &
+if type "xrandr"; then
+  for m in $(polybar --list-monitors | cut -d":" -f1); do
+      MONITOR=$m polybar --reload example &
+  done
+else
+  polybar --reload example &
+fi
